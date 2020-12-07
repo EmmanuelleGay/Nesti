7071ccfa529a351b65491ee0d5e026fe5afd0bd2
Manu <emmanuellegay.13@gmail.com>
2020-12-07 19:50:12 +0100

    fonction pour vérfiier le hash password au moment du log

--- src/nesti/HashPassword.java
+++ src/nesti/HashPassword.java
@@ -7,19 +7,28 @@ import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
-/*
- * PBKDF2 salted password hashing.
- * Author: havoc AT defuse.ca
- * www: http://crackstation.net/hashing-security.htm
- */
-public class HashPassword {
 
+public class HashPassword {
+	
+	/**
+	 * Create hash and salt password
+	 * @param member
+	 * @throws NoSuchAlgorithmException
+	 * @throws InvalidKeySpecException
+	 */
 	public static void generateHashPassword(Member member) throws NoSuchAlgorithmException, InvalidKeySpecException{
 		String originalPassword = member.getPassword();
 		String generatedSecuredPasswordHash = generateStorngPasswordHash(originalPassword);
 		member.setPassword(generatedSecuredPasswordHash);
 	}
 	
+	/**
+	 * 
+	 * @param password
+	 * @return iterations + ":" + toHex(salt) + ":" + toHex(hash
+	 * @throws NoSuchAlgorithmException
+	 * @throws InvalidKeySpecException
+	 */
 	private static String generateStorngPasswordHash(String password)
 			throws NoSuchAlgorithmException, InvalidKeySpecException {
 		int iterations = 1000;
@@ -32,6 +41,11 @@ public class HashPassword {
 		return iterations + ":" + toHex(salt) + ":" + toHex(hash);
 	}
 
+	/**
+	 * Salt password
+	 * @return salt
+	 * @throws NoSuchAlgorithmException
+	 */
 	private static byte[] getSalt() throws NoSuchAlgorithmException {
 		SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
 		byte[] salt = new byte[16];
@@ -39,6 +53,12 @@ public class HashPassword {
 		return salt;
 	}
 
+	/**
+	 * used to hash password
+	 * @param array
+	 * @return
+	 * @throws NoSuchAlgorithmException
+	 */
 	private static String toHex(byte[] array) throws NoSuchAlgorithmException {
 		BigInteger bi = new BigInteger(1, array);
 		String hex = bi.toString(16);
@@ -49,4 +69,55 @@ public class HashPassword {
 			return hex;
 		}
 	}
+	
+
+/**
+ * Used to validate the password again when user comes back and login
+ * @param originalPassword
+ * @param storedPassword
+ * @return diff
+ * @throws NoSuchAlgorithmException
+ * @throws InvalidKeySpecException
+ */
+
+    public static boolean validatePassword(String originalPassword, String storedPassword) throws NoSuchAlgorithmException, InvalidKeySpecException
+    {
+        String[] parts = storedPassword.split(":");
+        int iterations = Integer.parseInt(parts[0]);
+        byte[] salt = fromHex(parts[1]);
+        byte[] hash = fromHex(parts[2]);
+         
+        PBEKeySpec spec = new PBEKeySpec(originalPassword.toCharArray(), salt, iterations, hash.length * 8);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] testHash = skf.generateSecret(spec).getEncoded();
+         
+        int diff = hash.length ^ testHash.length;
+        for(int i = 0; i < hash.length && i < testHash.length; i++)
+        {
+            diff |= hash[i] ^ testHash[i];
+        }
+        return diff == 0;
+    }
+    
+  /**
+   *  used to recover hash password
+   * @param hex
+   * @return bytes
+   * @throws NoSuchAlgorithmException
+   */
+    private static byte[] fromHex(String hex) throws NoSuchAlgorithmException
+    {
+        byte[] bytes = new byte[hex.length() / 2];
+        for(int i = 0; i<bytes.length ;i++)
+        {
+            bytes[i] = (byte)Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
+        }
+        return bytes;
+    }
+ 
 }
+
+
+	
+	
+	
