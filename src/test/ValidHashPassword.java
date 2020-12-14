837520d703a90012ff39346978860555ffc50ddc
Manu <emmanuellegay.13@gmail.com>
2020-12-14 14:15:28 +0100

    class tests password hash

---
+++ src/test/ValidHashPassword.java
@@ -0,0 +1,32 @@
+package test;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import junit.framework.TestCase;
+import nesti.HashPassword;
+
+public class ValidHashPassword extends TestCase {
+	
+	HashPassword validHashPassword = new HashPassword();
+	String hashPassword;
+	
+	
+	public void testValidatePassword() throws NoSuchAlgorithmException, InvalidKeySpecException{
+		boolean matched;
+		
+		validHashPassword.generateHashPassword("Azerty34!");
+		
+		matched = HashPassword.validatePassword("Azerty34!",validHashPassword.getHashPassword());
+		assertTrue(matched);
+		
+		matched = HashPassword.validatePassword("Azerty36!",validHashPassword.getHashPassword());
+		assertFalse(matched);
+		
+		matched = HashPassword.validatePassword("123456",validHashPassword.getHashPassword());
+		assertFalse(matched);
+		
+	}
+	
+	
+}
