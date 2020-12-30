2064f9e8c870a0e16c965892a3fe29cd3654bc2b
Manu <emmanuellegay.13@gmail.com>
2020-12-30 14:44:09 +0100

    ajout d'un bouton connexion

--- src/nesti/ViewPasswordForget.java
+++ src/nesti/ViewPasswordForget.java
@@ -2,6 +2,8 @@ package nesti;
 
 import java.awt.BorderLayout;
 import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -38,6 +40,17 @@ public class ViewPasswordForget {
 
 		Buttons btnSubmit = new Buttons("Valider",703, 450, 108, 35);
 		containerForm.add(btnSubmit);
+		
+		Buttons btnConnexion = new Buttons("Connexion",998, 96, 108, 42);
+		btnConnexion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				frame.dispose();
+				ViewLogin viewLogin = new ViewLogin();
+
+			}
+		});
+		
+		containerForm.add(btnConnexion);
 		
 		JPanel ContaineurLogo = new JPanel();
 		ContaineurLogo.setBackground(SystemColor.window);
